62d718ee378529f75ed8b20f76788a352bc653c9
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2022-02-26 22:02:34 -0300

    Fix game logic

--- src/wordle/core.cljs
+++ src/wordle/core.cljs
@@ -6,6 +6,8 @@
 (def attempt (atom 0))
 (def word-of-the-day (atom (rand-nth (seq words))))
 
+(println @word-of-the-day)
+
 (defn write-letter [cell letter]
   (set! (.-textContent cell) letter))
 
@@ -26,19 +28,47 @@
 (defn get-letter [cell]
   (.-textContent cell))
 
-(defn color-cell [idx cell]
-  (let [letter (get-letter cell)
-        add-class (fn [el class] (.add (.-classList el) class))]
-    (cond
-      (= letter (nth @word-of-the-day idx)) (add-class cell "right")
-      (contains? (set @word-of-the-day) letter) (add-class cell "missplaced")
-      :else (add-class cell "wrong"))))
+(defn color-cells! [cells difference]
+  (let [add-class (fn [el class] (.add (.-classList el) class))]
+    (doseq [[idx cell] (map-indexed vector cells)]
+      (condp = (nth difference idx)
+        :exact (add-class cell "exact")
+        :misplaced (add-class cell "misplaced")
+        :miss (add-class cell "miss")))))
+
+(defn letters-in-right-place
+  "Returns a vector containing :miss or :exact for each letter position"
+  [guess solution]
+  (->> (map-indexed (fn [idx _]
+                      (cond
+                        (= (nth guess idx) (nth solution idx)) :exact
+                        :else :miss))
+                    guess)
+       (apply vector)))
+
+(defn set-misplaced-letters!
+  "Return a vector containing :misplaced for the letters that are misplaced"
+  [guess solution result]
+  (let [guess-atom (atom @result)]
+    (doseq [i (range (count @result))]
+      (when-not (= :exact (nth @result i))
+        (doseq [j (range (count solution))]
+          (when (and (not= :misplaced (nth @result j)) (not= :exact (nth @result j)) (not= :misplaced (nth @guess-atom i)) (= (nth guess i) (nth solution j)))
+            (swap! guess-atom assoc i :misplaced)
+            (swap! result assoc j :misplaced)))))
+    @guess-atom))
+
+(defn check-words-difference
+  [guess solution]
+  (let [result (atom (letters-in-right-place guess solution))]
+    (set-misplaced-letters! guess solution result)))
 
 (defn check-solution [cells]
-  (doseq [[idx cell] (map-indexed vector cells)]
-    (color-cell idx cell))
-  (= (mapv get-letter cells)
-     (vec @word-of-the-day)))
+  (let [difference (check-words-difference (mapv get-letter cells) (vec @word-of-the-day))]
+    (color-cells! cells difference)
+    (every? #(= :exact %) difference)))
+
+(defonce listener (atom nil))
 
 (defn user-input [key]
   (let [start (* 5 @attempt)
@@ -53,10 +83,9 @@
                                    (write-letter (nth @board-state @counter) ""))
       (and (= key "enter")
            (zero? (mod @counter 5))) (when-not (check-solution (subvec @board-state start end))
-                                       (when-not (= 5 @attempt)
+                                       (if (= 5 @attempt)
+                                         (js/document.removeEventListener "keydown" @listener)
                                          (swap! attempt inc))))))
-
-(defonce listener (atom nil))
 
 (defn ^:dev/before-load unmount []
   (js/document.removeEventListener "keydown" @listener)
@@ -75,3 +104,10 @@
      input-listener)))
 
 (mount)
+
+
+(comment
+
+
+
+  (check-words-difference (list "y" "e" "t") (list "h" "e" "y")))
